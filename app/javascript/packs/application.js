f8bea349fbff80a34d14e38f6ba15fb5c53a2990
Renan Martins <renanmartins989@gmail.com>
2022-09-28 05:01:17 +0100

    js working using vanilla js still with the same error message on console

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,8 +3,10 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("jquery")
+require("bootstrap")
 
-
+import { initSmoothScroll } from "./plugins/smoothScroll"
 
 // import Rails from "@rails/ujs"
 // import Turbolinks from "turbolinks"
@@ -20,13 +22,13 @@ require("channels")
 // import "channels"
 
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+// Rails.start()
+// Turbolinks.start()
+// ActiveStorage.start()
 
 
 document.addEventListener('turbolinks:load', () => {
-
+  initSmoothScroll()
 });
 
 // Configure Stimulus development experience
