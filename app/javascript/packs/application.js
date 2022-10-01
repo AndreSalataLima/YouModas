d06b63e5b29d102e54ab801bdd2dddb4b8d4d8ea
Renan Martins <renanmartins989@gmail.com>
2022-10-01 17:09:27 +0100

    just removing commented code from app.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,30 +9,7 @@ require("util")
 import { initSmoothScroll } from "./plugins/smoothScroll"
 import { initNavShow } from "./plugins/initNavShow"
 
-// import Rails from "@rails/ujs"
-// import Turbolinks from "turbolinks"
-// import * as ActiveStorage from "@rails/activestorage"
-// require("@fortawesome/fontawesome-free")
-
-// import "bootstrap"
-// import "jquery3"
-// import "popper"
-// import "bootstrap-sprockets"
-// Use sprockets for inidividual bootstrap components for easy debugging
-// import "controllers"
-// import "channels"
-
-// initSmoothScroll();
-// Rails.start()
-// Turbolinks.start()
-// ActiveStorage.start()
 
 document.addEventListener('turbolinks:load', () => {
-  // initSmoothScroll()
+
 });
-
-// Configure Stimulus development experience
-// application.debug = false
-// window.Stimulus   = application
-
-// export { application }
