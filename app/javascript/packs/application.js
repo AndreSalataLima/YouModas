bb1cde803bb12779c800df730aa04ba7f3b678ee
Renan Martins <91997504+nanmartins@users.noreply.github.com>
2022-10-23 06:34:37 +0100

    slide is basics done, just waiting to know if we going to use it or not, if yes, improve style and responsiv. Carousel tests looks good as well

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ require("util")
 import { initSmoothScroll } from "./plugins/smoothScroll"
 import { initNavShow } from "./plugins/initNavShow"
 import { initSlides } from "./plugins/initSlides"
+import { carouselTest } from "./plugins/carouselTest"
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
